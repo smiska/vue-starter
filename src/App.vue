d51dd669e573446b48087a5d95963bf24dc64d1e
smiska <Mihly Sry>
2019-10-22 09:24:07 +0200

    modal works, table assemled

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,115 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>KonaSoft App</h1>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <h3 v-if="error">{{ error }}</h3>
+    <TableDisplay
+      @showModal="onShowModal"
+      v-bind:users="users"
+      v-bind:columns="columns"
+      v-bind:filterText="filterText"
+      v-else
+    ></TableDisplay>
+    <Modal @showModal="onShowModal" v-if="showModal"></Modal>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TableDisplay from "./components/TableDisplay.vue";
+import Modal from "./components/Modal.vue";
+import axios from "axios";
+
+const ITEM = {
+  ID: "Id",
+  NAME: "Nev",
+  PERM: "Jogosultságok",
+  AGE: "Eletkor",
+  CREATED_AT: "Regisztralt",
+  COMPANY: "Munkahely"
+};
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      filterText: "",
+      columns: [],
+      users: [],
+      error: "",
+      showModal: false
+    };
+  },
+  methods: {
+    onShowModal() {
+      this.showModal = !this.showModal;
+    },
+    mapToEnglish(users) {
+      return users.map(user => {
+        this.validateUser(user);
+
+        function extractInfo(items) {
+          if (!Array.isArray(items)) {
+            items = [items];
+          }
+          return items.map(i => i[ITEM.NAME]);
+        }
+
+        return (user = {
+          id: user[ITEM.ID],
+          name: user[ITEM.NAME],
+          permissions:
+            typeof user[ITEM.PERM] === "object"
+              ? extractInfo(user[ITEM.PERM])
+              : user[ITEM.PERM],
+          age: user[ITEM.AGE],
+          createdAt: user[ITEM.CREATED_AT],
+          company:
+            typeof user[ITEM.COMPANY] === "object"
+              ? extractInfo(user[ITEM.COMPANY])
+              : user[ITEM.COMPANY]
+        });
+      });
+    },
+    validateUser(user) {
+      if (!(user["Id"] && user["Nev"])) {
+        throw new Error("Invalid DB data"); // throwing error in for loop?
+      }
+    }
+  },
+
+  async mounted() {
+    try {
+      const response = await axios.get("./db.json");
+      const users = response.data;
+
+      // assign original DB user keys
+      this.columns = Object.keys(users[0]);
+
+      // use mapped properties for users
+      this.users = this.mapToEnglish(users);
+    } catch (error) {
+      // TODO - error handling gracefully
+      this.error = error;
+    }
+  },
   components: {
-    HelloWorld
+    TableDisplay,
+    Modal
   }
-}
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 60px;
+
+  h1 {
+    margin-bottom: 30px;
+  }
 }
 </style>
---
+++ src/components/TableDisplay.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="table">
+    <div class="thead">
+      <div v-for="column in columns" class="tcell">{{ column }}</div>
+    </div>
+    <div class="tbody">
+      <div class="trow" @click="editRow" v-for="user in users" :key="user.id">
+        <div class="tcell" v-for="value in user">{{ value }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TableDisplay",
+  props: {
+    users: Array,
+    columns: Array,
+    filterText: String
+  },
+  methods: {
+    editRow(event) {
+      this.$emit("showModal", event.target.p);
+      console.log(event);
+    }
+  },
+  components: {}
+};
+</script>
+
+<style lang="scss" scoped>
+* {
+  box-sizing: border-box;
+}
+
+.table {
+  width: 76%;
+  height: 100%;
+  margin: 0 auto;
+}
+
+.table .thead {
+  .tcell {
+    height: 5em;
+  }
+}
+
+.thead,
+.trow {
+  display: flex;
+  align-content: space-evenly;
+  background-color: #ffffff;
+}
+
+.trow {
+  &:hover {
+    background: rgba(255, 255, 255, 0.7);
+    cursor: pointer;
+    .tcell {
+      background-color: #ffffff;
+      // border: 2px solid #ebebeb;
+    }
+  }
+}
+
+.tcell {
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  width: 10em;
+  height: 4em;
+  margin: 0.1em;
+  border: 2px solid #ebebeb;
+}
+</style>
